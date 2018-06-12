768b88be9411408076fe765e05b47438c09782d3
dcsheive <dcsheive@gmail.com>
2018-06-12 13:34:28 -0400

    Message Board

---
+++ message_board/server.js
@@ -0,0 +1,98 @@
+// Require the Express Module
+var express = require('express');
+// Create an Express App
+var app = express();
+// Require body-parser (to receive post data from clients)
+var bodyParser = require('body-parser');
+
+var session = require('express-session')
+app.use(session({
+    secret: 'codingmojo',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}))
+
+const flash = require('express-flash');
+app.use(flash());
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/messages');
+var CommentSchema = new mongoose.Schema({
+    name:  { type: String, required: true, minlength: 3},
+    text: { type: String, required: true, maxlength: 200, minlength:1 },
+}, {timestamps: true });
+var MessageSchema = new mongoose.Schema({
+    name:  { type: String, required: true, minlength: 3},
+    text: { type: String, required: true, maxlength: 200, minlength:1 },
+    comments:[CommentSchema]
+}, {timestamps: true });
+mongoose.Promise = global.Promise;
+
+mongoose.model('Message', MessageSchema); // We are setting this Schema in our Models as 'User'
+mongoose.model('Comment', CommentSchema); // We are setting this Schema in our Models as 'User'
+var Message = mongoose.model('Message') // We are retrieving this Schema from our Models, named 'User'
+var Comment = mongoose.model('Comment') // We are retrieving this Schema from our Models, named 'User'
+
+// Integrate body-parser with our App
+app.use(bodyParser.urlencoded({ extended: true }));
+// Require path
+var path = require('path');
+// Setting our Static Folder Directory
+app.use(express.static(path.join(__dirname, './static')));
+// Setting our Views Folder Directory
+app.set('views', path.join(__dirname, './views'));
+// Setting our View Engine set to EJS
+app.set('view engine', 'ejs');
+// Routes
+// Root Request
+app.get('/', function(req, res) {
+    Message.find({},function(err, messages){
+        return res.render('index', {messages:messages})
+    })
+
+})
+// Add User Request 
+app.post('/message', function (req, res){
+    var message = new Message(req.body);
+    message.save(function(err){
+        if(err){
+            // if there is an error upon saving, use console.log to see what is in the err object 
+            console.log("We have an error!", err);
+            // adjust the code below as needed to create a flash message with the tag and content you would like
+            for(var key in err.errors){
+                req.flash('message', err.errors[key].message);
+            }
+            // redirect the user to an appropriate route
+            res.redirect('/');
+        }
+        else {
+            res.redirect('/');
+        }
+    });
+});
+app.post('/comment', function (req, res){
+    var comment = new Comment(req.body);
+    Message.findById(req.body.message_id, function(err, message){
+        message.comments.push(comment)
+        message.save(function(err){
+            if(err){
+                // if there is an error upon saving, use console.log to see what is in the err object 
+                console.log("We have an error!", err);
+                // adjust the code below as needed to create a flash message with the tag and content you would like
+                for(var key in err.errors){
+                    req.flash('comment', err.errors[key].message);
+                }
+                // redirect the user to an appropriate route
+                res.redirect('/');
+            }
+            else {
+                res.redirect('/')
+            }
+        })
+    })
+});
+// Setting our Server to Listen on Port: 8000
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+})
