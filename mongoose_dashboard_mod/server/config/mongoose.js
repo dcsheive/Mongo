abb1553caf3971c7f99dfe9d5fcd46a83a09298f
dcsheive <dcsheive@gmail.com>
2018-06-13 16:28:01 -0400

    changed db in mongoose

--- mongoose_dashboard_mod/server/config/mongoose.js
+++ mongoose_dashboard_mod/server/config/mongoose.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs')
 const mongoose = require('mongoose');
 var models_path = path.join(__dirname, './../models');
-mongoose.connect('mongodb://localhost/quotes');
+mongoose.connect('mongodb://localhost/owls');
 mongoose.Promise = global.Promise;
 
 fs.readdirSync(models_path).forEach(function(file) {
